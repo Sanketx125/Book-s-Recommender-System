c8b83d8491b7d7a25185c06b1998dad647c9b81a
Sanket Mane <103379134+Sanketx125@users.noreply.github.com>
2024-11-02 16:43:35 +0530

    Update app.py

--- website/app.py
+++ website/app.py
@@ -34,7 +34,7 @@ def recommend():
         
         try:
             index = np.where(pt.index == user_input)[0][0]
-            similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[1:7]
+            similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[0:7]
 
             data = []
             for i in similar_items:
